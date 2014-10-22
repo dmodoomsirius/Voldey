32c61a298433e3396033f199bb3d5377a33ff837
James <lords-death@hotmail.com>
2014-10-22 16:09:14 +1300

    Updated join
    Support for mutiple masks

--- plugins/join.py
+++ plugins/join.py
@@ -3,8 +3,9 @@ import time
 
 @hook.irc_raw("JOIN")
 def join(message, nick, mask, notice):
-	if "Mibbit" in mask:
+	noobs = ['Mibbit', 'webchat']
+	if any(x in mask for x in noobs):	
 		time.sleep(2)
 		message("Hello and welcome to #Cauldron IRC "+nick+". Please read http://cauldron.minecraftforge.net/threads/irc-guidelines.18/ BEFORE doing anything else")
-		message("Cauldron has been DMCAed, don't ask about downloads|The future of servers is uncertain")
+		message("Cauldron has been DMCAed, check the topic (/topic) for info about downloads|The future of servers is uncertain")
 		notice(nick +" You really want to read that link, you might get kicked otherwise (there are instructions to preform)")
