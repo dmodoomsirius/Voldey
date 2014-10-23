3908976ceeb18763c992aef3bdeb72ac7bbb9bea
James <lords-death@hotmail.com>
2014-10-24 12:20:44 +1300

    updated brycey
    Start of line support

--- plugins/brycey.py
+++ plugins/brycey.py
@@ -1,7 +1,7 @@
 from cloudbot import hook
 import re
 
-matchString = re.compile('(\.yay)|(\.rage)')
+matchString = re.compile('^(\.yay)|^(\.rage)')
 @hook.regex(matchString)
 def brycey(nick, match, notice):
 		notice(nick +" I can spam annoying commands as well")
