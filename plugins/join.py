782e9a40a9c7a358808e35b55c0079324c45c6e6
James <lords-death@hotmail.com>
2014-11-19 22:05:42 +1300

    join update
    Now has channel filtering

--- plugins/join.py
+++ plugins/join.py
@@ -2,10 +2,11 @@ from cloudbot import hook
 import time
 
 @hook.irc_raw("JOIN")
-def join(message, nick, mask, notice):
+def join(message, nick, mask, notice, chan):
 	noobs = ['Mibbit', 'webchat']
-	if any(x in mask for x in noobs):	
-		time.sleep(2)
-		message("Hello and welcome to #Cauldron IRC "+nick+". Please read http://cauldron.minecraftforge.net/threads/irc-guidelines.18/ BEFORE doing anything else")
-		message("Cauldron has been DMCAed, check the topic (/topic) for info about downloads|The future of servers is uncertain")
-		notice(nick +" You really want to read that link, you might get kicked otherwise (there are instructions to preform)")
+	if chan == "#cauldron":
+                if any(x in mask for x in noobs):	
+                        time.sleep(2)
+                        message("Hello and welcome to #Cauldron IRC "+nick+". Please read http://cauldron.minecraftforge.net/threads/irc-guidelines.18/ BEFORE doing anything else")
+                        message("Cauldron has been DMCAed, check the topic (/topic) for info about downloads|The future of servers is uncertain")
+                        notice(nick +" You really want to read that link, you might get kicked otherwise (there are instructions to preform)")
