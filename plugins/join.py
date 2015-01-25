1327de8d7f03ff9b53e5ab2124ef3549100e2c02
James <lords-death@hotmail.com>
2015-01-25 21:25:22 +1300

    Updated plugins

--- plugins/join.py
+++ plugins/join.py
@@ -8,5 +8,4 @@ def join(message, nick, mask, notice, chan):
                 if any(x in mask for x in noobs):	
                         time.sleep(2)
                         message("Hello and welcome to #Cauldron IRC "+nick+". Please read http://cauldron.minecraftforge.net/threads/irc-guidelines.18/ BEFORE doing anything else")
-                        message("Cauldron has been DMCAed, check the topic (/topic) for info about downloads|The future of servers is uncertain")
-                        notice(nick +" You really want to read that link, you might get kicked otherwise (there are instructions to preform)")
+                        message("Cauldron is dead don't ask about downloads, support isn't guaranteed")
