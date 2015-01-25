faf03a62e3b934739df3c28da7b03a5988aa32bb
James <lords-death@hotmail.com>
2015-01-25 23:04:20 +1300

    Updated filter allow users with the right permissions to ignore the filter

--- plugins/spongeFilter.py
+++ plugins/spongeFilter.py
@@ -4,7 +4,7 @@ from cloudbot import hook
 def spongeSieve(bot, input, plugin):
 	#print(dir(plugin)) #This will show all attributes for an object
 	if "#sponge" in input.chan:
-		if plugin.function_name == "flard":
+		if "flard" in plugin.function_name || has_permission("ignore"):
 			return input
 		else:
 			return None
